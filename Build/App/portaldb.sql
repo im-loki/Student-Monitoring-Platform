fa846efb68c3d8e4e628d9946d78df2a6ab69b98
im-loki <37450759+im-loki@users.noreply.github.com>
2018-10-20 14:05:27 +0530

    changes to er-diagram

--- Build/App/portaldb.sql
+++ Build/App/portaldb.sql
@@ -1,55 +1,52 @@
-CREATE TABLE `class`(
-    `sec` CHAR(1) NOT NULL,
-    `sem` CHAR(1) NOT NULL,
-    PRIMARY KEY(`sem`, `sec`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `course`(
-    `cin` VARCHAR(7) NOT NULL,
-    `NAME` VARCHAR(20) DEFAULT NULL,
-    PRIMARY KEY(`cin`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `enroll`(
-    `cin` VARCHAR(7) NOT NULL,
-    `usn` CHAR(10) NOT NULL,
-    `sec` CHAR(1) NOT NULL,
-    `sem` CHAR(1) NOT NULL,
-    PRIMARY KEY(`cin`, `usn`, `sec`, `sem`),
-    KEY `usn2_fk`(`usn`),
-    KEY `sec01_fk`(`sem`, `sec`),
-    CONSTRAINT `cin1_fk` FOREIGN KEY(`cin`) REFERENCES `course`(`cin`) ON DELETE CASCADE,
-    CONSTRAINT `sec01_fk` FOREIGN KEY(`sem`, `sec`) REFERENCES `class`(`sem`, `sec`) ON DELETE CASCADE,
-    CONSTRAINT `usn2_fk` FOREIGN KEY(`usn`) REFERENCES `student`(`usn`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `student`(
-    `usn` CHAR(10) NOT NULL,
-    `NAME` VARCHAR(20) DEFAULT NULL,
-    `sec` CHAR(1) DEFAULT NULL,
-    `sem` CHAR(1) DEFAULT NULL,
-    `ssn` CHAR(10) DEFAULT NULL,
-    PRIMARY KEY(`usn`),
-    KEY `ssn_fk`(`ssn`),
-    KEY `sec_fk`(`sem`, `sec`),
-    CONSTRAINT `sec_fk` FOREIGN KEY(`sem`, `sec`) REFERENCES `class`(`sem`, `sec`) ON DELETE CASCADE,
-    CONSTRAINT `ssn_fk` FOREIGN KEY(`ssn`) REFERENCES `teacher`(`ssn`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `student_attendance`(
-    `usn` CHAR(10) NOT NULL,
-    `attendance` FLOAT DEFAULT NULL,
-    PRIMARY KEY(`usn`),
-    CONSTRAINT `usn1_fk` FOREIGN KEY(`usn`) REFERENCES `student`(`usn`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `student_marks`(
-    `usn` CHAR(10) NOT NULL,
-    `test1` FLOAT DEFAULT NULL,
-    `test2` FLOAT DEFAULT NULL,
-    `test3` FLOAT DEFAULT NULL,
-    `finalia` FLOAT DEFAULT NULL,
-    PRIMARY KEY(`usn`),
-    CONSTRAINT `usn_fk` FOREIGN KEY(`usn`) REFERENCES `student`(`usn`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `teacher`(
-    `ssn` CHAR(10) NOT NULL,
-    `NAME` VARCHAR(20) DEFAULT NULL,
-    PRIMARY KEY(`ssn`)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `teacher_offer`(
-    `ssn` CHAR(10) NOT NULL,
-    `cin` VARCHAR(7) NOT NULL,
-    PRIMARY KEY(`ssn`, `cin`),
-    KEY `cin_fk`(`cin`),
-    CONSTRAINT `cin_fk` FOREIGN KEY(`cin`) REFERENCES `course`(`cin`) ON DELETE CASCADE,
-    CONSTRAINT `ssn1_fk` FOREIGN KEY(`ssn`) REFERENCES `teacher`(`ssn`) ON DELETE CASCADE
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE class(
+    sem VARCHAR(5),
+    sec VARCHAR(5),
+    CONSTRAINT pk_01 PRIMARY KEY(sem, sec)
+); CREATE TABLE teacher(
+    ssn VARCHAR(5),
+    NAME VARCHAR(20),
+    CONSTRAINT pk_02 PRIMARY KEY(ssn)
+); CREATE TABLE course(
+    cin VARCHAR(5),
+    NAME VARCHAR(20),
+    credits INTEGER,
+    sem VARCHAR(5),
+    CONSTRAINT pk_03 PRIMARY KEY(cin),
+    CONSTRAINT fk_01 FOREIGN KEY(sem) REFERENCES class(sem) ON DELETE CASCADE
+); CREATE TABLE student(
+    usn VARCHAR(10),
+    NAME VARCHAR(20),
+    sem VARCHAR(5),
+    sec VARCHAR(5),
+    ssn VARCHAR(5),
+    CONSTRAINT pk_04 PRIMARY KEY(usn),
+    CONSTRAINT fk_02 FOREIGN KEY(sem, sec) REFERENCES class(sem, sec),
+    CONSTRAINT fk_03 FOREIGN KEY(ssn) REFERENCES teacher(ssn)
+); CREATE TABLE attandance(
+    attandance FLOAT,
+    cin VARCHAR(5),
+    usn VARCHAR(10),
+    CONSTRAINT fk_04 FOREIGN KEY(cin) REFERENCES course(cin),
+    CONSTRAINT fk_05 FOREIGN KEY(usn) REFERENCES student(usn),
+    CONSTRAINT pk_05 PRIMARY KEY(cin, usn)
+); CREATE TABLE marks(
+    test1 FLOAT,
+    test2 FLOAT,
+    test3 FLOAT,
+    finalia FLOAT,
+    cin VARCHAR(5),
+    usn VARCHAR(5),
+    CONSTRAINT fk_06 FOREIGN KEY(cin) REFERENCES course(cin),
+    CONSTRAINT fk_07 FOREIGN KEY(usn) REFERENCES student(usn),
+    CONSTRAINT pk_06 PRIMARY KEY(cin, usn)
+); CREATE TABLE teaches(
+    ssn VARCHAR(5),
+    cin VARCHAR(5),
+    CONSTRAINT fk_08 FOREIGN KEY(ssn) REFERENCES teacher(ssn),
+    CONSTRAINT fk_09 FOREIGN KEY(cin) REFERENCES course(cin),
+    CONSTRAINT pk_07 PRIMARY KEY(ssn, cin)
+); CREATE TABLE m_reports(
+    usn VARCHAR(10),
+    comments VARCHAR(100),
+    CONSTRAINT fk_10 FOREIGN KEY(usn) REFERENCES student(usn)
+);
