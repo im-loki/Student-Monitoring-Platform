071ff2917efd4d249a356be47485e76ee40925d7
im-loki <37450759+im-loki@users.noreply.github.com>
2018-10-20 12:07:30 +0530

    changed a few parameters
    added test1,2,3 finalia for student_marks

--- Build/App/portaldb.sql
+++ Build/App/portaldb.sql
@@ -1,15 +1,55 @@
-create table class(sec char(1),sem char(1),constraint sec_pk primary key(sec),constraint sem_pk primary key(sem));
-
-create table teacher(ssn char(10),name varchar(20),constraint ssn_pk primary key(ssn));
-
-create table student(usn char(10),name varchar(20),sec char(1),sem char(1),ssn char(10),constraint usn_pk primary key(usn),constraint ssn_fk foreign key(ssn) references teacher(ssn) on delete cascade,constraint sec_fk foreign key(sec) references class(sec) on delete cascade,constraint sem_fk foreign key(sem) references class(sem) on delete cascade);
-
-create table student_marks(usn char(10),marks number,constraint usn1_pk primary key(usn),constraint usn_fk foreign key(usn) references student(usn) on delete cascade);
-
-create table student_attendance(usn char(10),attendance number,constraint usn2_pk primary key(usn),constraint usn1_fk foreign key(usn) references student(usn) on delete cascade);
-
-create table course(cin varchar(7),name varchar(20),constraint cin_pk primary key(cin));
-
-create table teacher_offer(ssn char(10),cin varchar(7),constraint offer_pk primary key(ssn,cin),constraint ssn1_fk foreign key(ssn) references teacher(ssn) on delete cascade,constraint cin_fk foreign key(cin) references course(cin) on delete cascade);
-
-create table enroll(cin varchar(7),usn char(10),sec char(1),sem char(1),constraint enroll_pk primary key(cin,usn,sec,sem),constraint cin1_fk foreign key(cin) references course(cin) on delete cascade,constraint usn2_fk foreign key(usn) references student(usn) on delete cascade,constraint sec_fk foreign key(sec) references class(sec) on delete cascade,constraint sem1_fk foreign key(sem) references class(sem) on delete cascade);
+CREATE TABLE `class`(
+    `sec` CHAR(1) NOT NULL,
+    `sem` CHAR(1) NOT NULL,
+    PRIMARY KEY(`sem`, `sec`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `course`(
+    `cin` VARCHAR(7) NOT NULL,
+    `NAME` VARCHAR(20) DEFAULT NULL,
+    PRIMARY KEY(`cin`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `enroll`(
+    `cin` VARCHAR(7) NOT NULL,
+    `usn` CHAR(10) NOT NULL,
+    `sec` CHAR(1) NOT NULL,
+    `sem` CHAR(1) NOT NULL,
+    PRIMARY KEY(`cin`, `usn`, `sec`, `sem`),
+    KEY `usn2_fk`(`usn`),
+    KEY `sec01_fk`(`sem`, `sec`),
+    CONSTRAINT `cin1_fk` FOREIGN KEY(`cin`) REFERENCES `course`(`cin`) ON DELETE CASCADE,
+    CONSTRAINT `sec01_fk` FOREIGN KEY(`sem`, `sec`) REFERENCES `class`(`sem`, `sec`) ON DELETE CASCADE,
+    CONSTRAINT `usn2_fk` FOREIGN KEY(`usn`) REFERENCES `student`(`usn`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `student`(
+    `usn` CHAR(10) NOT NULL,
+    `NAME` VARCHAR(20) DEFAULT NULL,
+    `sec` CHAR(1) DEFAULT NULL,
+    `sem` CHAR(1) DEFAULT NULL,
+    `ssn` CHAR(10) DEFAULT NULL,
+    PRIMARY KEY(`usn`),
+    KEY `ssn_fk`(`ssn`),
+    KEY `sec_fk`(`sem`, `sec`),
+    CONSTRAINT `sec_fk` FOREIGN KEY(`sem`, `sec`) REFERENCES `class`(`sem`, `sec`) ON DELETE CASCADE,
+    CONSTRAINT `ssn_fk` FOREIGN KEY(`ssn`) REFERENCES `teacher`(`ssn`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `student_attendance`(
+    `usn` CHAR(10) NOT NULL,
+    `attendance` FLOAT DEFAULT NULL,
+    PRIMARY KEY(`usn`),
+    CONSTRAINT `usn1_fk` FOREIGN KEY(`usn`) REFERENCES `student`(`usn`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `student_marks`(
+    `usn` CHAR(10) NOT NULL,
+    `test1` FLOAT DEFAULT NULL,
+    `test2` FLOAT DEFAULT NULL,
+    `test3` FLOAT DEFAULT NULL,
+    `finalia` FLOAT DEFAULT NULL,
+    PRIMARY KEY(`usn`),
+    CONSTRAINT `usn_fk` FOREIGN KEY(`usn`) REFERENCES `student`(`usn`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `teacher`(
+    `ssn` CHAR(10) NOT NULL,
+    `NAME` VARCHAR(20) DEFAULT NULL,
+    PRIMARY KEY(`ssn`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; CREATE TABLE `teacher_offer`(
+    `ssn` CHAR(10) NOT NULL,
+    `cin` VARCHAR(7) NOT NULL,
+    PRIMARY KEY(`ssn`, `cin`),
+    KEY `cin_fk`(`cin`),
+    CONSTRAINT `cin_fk` FOREIGN KEY(`cin`) REFERENCES `course`(`cin`) ON DELETE CASCADE,
+    CONSTRAINT `ssn1_fk` FOREIGN KEY(`ssn`) REFERENCES `teacher`(`ssn`) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
