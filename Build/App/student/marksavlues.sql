6c3f633739374e0aca0994173a79547089442432
guthal <37868861+guthal@users.noreply.github.com>
2018-11-08 20:32:15 +0530

    done with all the insertion

---
+++ Build/App/student/marksavlues.sql
@@ -0,0 +1,18 @@
+insert into marks values('20','12','10','0','15cs1','1bi16cs001');
+insert into marks values('20','12','12','0','15cs1','1bi16cs108');
+insert into marks values('12','12','12','0','15cs2','1bi16cs143');
+insert into marks values('20','20','20','0','15cs1','1bi16cs143');
+insert into marks values('17','15','12','0','15cs2','1bi16cs160');
+insert into marks values('12','12','12','0','15cs2','1bi16cs169');
+insert into marks values('14','12','15','0','15cs2','1bi16cs173');
+insert into marks values('15','14','20','0','15cs2','1bi16cs181');
+/*
+insert into attandance values('40','15cs1','1bi16cs001');
+insert into attandance values('60','15cs1','1bi16cs108');
+insert into attandance values('70','15cs2','1bi16cs143');
+insert into attandance values('70','15cs1','1bi16cs143');	
+insert into attandance values('33.3','15cs2','1bi16cs160');
+insert into attandance values('80','15cs2','1bi16cs169');
+insert into attandance values('88.8','15cs2','1bi16cs173');
+insert into attandance values('90','15cs2','1bi16cs181');
+*/
