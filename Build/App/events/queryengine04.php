18ebc4eabe6637bf3efbc5f6810ae2308bbd3918
im-loki <37450759+im-loki@users.noreply.github.com>
2018-11-21 00:20:43 +0530

    finalv1.03
    Added Stored Procedure to MySQL for the function of returning the events a student is partcipating.

--- Build/App/events/queryengine04.php
+++ Build/App/events/queryengine04.php
@@ -1,4 +1,4 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 <style>
@@ -6,56 +6,47 @@ table {
     width: 100%;
     border-collapse: collapse;
     text-align: left;
+    background-color: bisque;
 }
-
 table, td, th {
     border: 1px solid black;
     padding: 5px;
 }
-
-th {text-align: left;}
+th {
+    text-align: left;
+    background-color: black; color: white;
+}
 </style>
 </head>
 <body>
-
- <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "student01";
-
-$q=$_GET['q'];
-echo "$q".", your enrolled events are:"."<br>"."<hr>";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT e.event_name FROM events_list AS e, `participates` AS p WHERE p.usn = '$q' AND p.event_id = e.Event_id";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    // get dynamic header of the from course table. Refer the forms of teacher for reference.
-    echo "<table style=\"background-color: bisque;\">
-    <tr>
-    <th style=\"background-color: black; color: white;\">Event Name</th>
-    </tr>";
-    while($row = $result->fetch_assoc()) {
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "student01";
+    $q=$_GET['q'];
+    echo "$q".", your enrolled events are:"."<br>"."<hr>";
+    $connection = mysqli_connect($servername,$username,$password,$dbname);
+    //run the store proc
+    $result = mysqli_query($connection, 
+    "CALL event_signup('$q',@out);") or die("Query fail: " . mysqli_error());
+    $row_cnt = mysqli_num_rows($result);
+    if($row_cnt>0){
+        echo "<table>
+        <tr>
+        <th>Event Name</th>
+        </tr>";
+        //loop the result set
+        while ($row = mysqli_fetch_array($result)){   
         echo "<tr>";
-        echo "<td>" . $row['event_name'] . "</td>";
+        echo "<td>" . $row[0] . "</td>";
         echo "</tr>";
+        }
+        echo "</table>";
     }
-    echo "</table>";    
-} else {
-    echo "0 results";
-}
-$conn->close();
-?> 
-
+    else{
+        echo "No Result. Please Sign up to events, and then try again.";
+    }
+    ?> 
 </body>
 </html> 
